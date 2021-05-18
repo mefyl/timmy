8e0dad892734fb9e9c67d6427f58d8e4acb33631
mefyl <mefyl@gruntech.org>
2021-05-18 10:51:46 +0200

    Weekday: Expose, add integer conversion.

--- lib/src/weekday.ml
+++ lib/src/weekday.ml
@@ -7,3 +7,15 @@ type t =
   | `Sat
   | `Sun
   ]
+
+let to_int ?(base = `Mon) weekday =
+  let to_int = function
+    | `Mon -> 0
+    | `Tue -> 1
+    | `Wed -> 2
+    | `Thu -> 3
+    | `Fri -> 4
+    | `Sat -> 5
+    | `Sun -> 6
+  in
+  (to_int weekday - to_int base + 7) mod 7
