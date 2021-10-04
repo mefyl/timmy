2ed5ff2be9825c7388aca952849942d0da6bbb67
mefyl <mefyl@gruntech.org>
2021-10-04 12:59:12 +0200

    Month: Document.

--- timmy/lib/src/month.mli
+++ timmy/lib/src/month.mli
@@ -1,3 +1,5 @@
+(** {1 Type} *)
+
 (** A month. *)
 type t =
   | January
@@ -13,14 +15,18 @@ type t =
   | November
   | December
 
-(** Schema mapping to month names as strings. *)
+(** Schema mapping months to their english names. *)
 val schema_string : t Schematic.Schema.t
 
-(** Schema mapping to integers, 1 being January and 12 December. *)
+(** Schema mapping months to integers, 1 being January and 12 December. *)
 val schema_int : t Schematic.Schema.t
 
 (** [schema] is [schema_int]. *)
 val schema : t Schematic.Schema.t
+
+(** {1 Scalar conversions} *)
+
+(** {2 Integer} *)
 
 (** [to_int month] is 1-based index of [month] in the year, ie. 1 is January and
     12 is December. *)
@@ -30,8 +36,12 @@ val to_int : t -> int
     January and 12 December. *)
 val of_int : int -> (t, string) Result.t
 
+(** {2 Pretty-print} *)
+
+(** [pp f month] pretty-prints [month] to [f] as its english name. *)
+val pp : t Fmt.t
+
+(** {2 String} *)
+
 (** [to_string month] is the english name of [month]. *)
 val to_string : t -> string
-
-(** [pp] pretty prints months as their english name. *)
-val pp : t Fmt.t
