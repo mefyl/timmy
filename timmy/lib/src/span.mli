824e85ab88ebec88a266aa813bf59495243267de
mefyl <mefyl@gruntech.org>
2021-10-04 13:08:43 +0200

    Span: Document.

--- timmy/lib/src/span.mli
+++ timmy/lib/src/span.mli
@@ -1,25 +1,50 @@
 open Acid
 
+(** {1 Type} *)
+
+(** A duration.
+
+    Spans may be negative. *)
 type t
 
+(** {1 Construction} *)
+
+(** [days n] is a duration of [n] days. *)
 val days : int -> t
 
+(** [secondsn n] is a duration of [n] seconds. *)
 val seconds : int -> t
 
+(** {1 Time manipulation} *)
+
+(** [to_days span] is the number of days in [span] floored. *)
 val to_days : t -> int
 
+(** [to_days span] is the number of seconds in [span] floored. *)
 val to_seconds : t -> int
 
+(** {2 Comparison} *)
+
+include Comparable.S with type t := t
+
+(** {2 Operators} *)
+
+(** Convenience module to only pull operators. *)
 module O : sig
   include Comparable.Infix with type t := t
 
+  (** [-span] is the opposite of [span]. *)
   val ( ~- ) : t -> t
 end
 
 include module type of O
 
-include Comparable.S with type t := t
+(** {1 Scalar conversions} *)
 
+(** {2 Ptime} *)
+
+(** [of_ptime ptime_span] is the span equivalent to [ptime_span]. *)
 val of_ptime : Ptime.Span.t -> t
 
+(** [to_ptime span] is the Ptime span equivalent to [span]. *)
 val to_ptime : t -> Ptime.Span.t
