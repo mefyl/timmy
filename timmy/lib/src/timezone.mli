2d024163a816a3b44656cd3aca678c716f2c91d3
Alix Mougenot <alix.mougenot@routine.co>
2022-04-11 16:17:43 +0200

    Tests and doc improvements for timezones

--- timmy/lib/src/timezone.mli
+++ timmy/lib/src/timezone.mli
@@ -1,7 +1,8 @@
-(** Timmy timezone implementation is available in two flavors: one static and
-    one system dependant. The static one can be created as an offset from UTC.
-    The native one will bind to the underlying system to accomodate for daylight
-    savings (or any other time transition implemented with a timezone shift).*)
+(** Timmy timezone implementation is available in two flavors: using a static
+    offset from UTC or by providing an implementation. Providing an
+    implementation can be used when the need for accomodating to daylight
+    savings is needed (or any other time transition implemented with a timezone
+    shift). See {timmy-jsoo} for a native Javascript implementation. *)
 
 (** {1 Type} *)
 
@@ -11,12 +12,12 @@ type t
 (** {1 Construction} *)
 
 (** [of_gmt_offset_seconds s] is a static timezone with offset [s] seconds from
-    UTC.*)
+    UTC. UTC + offset = local.*)
 val of_gmt_offset_seconds : int -> t
 
 (** [of_implementation ~offset_calendar_time_s ~offset_timestamp_s] builds a
-    timezone by providing an implementation of the offset computation fromt a
-    date and from a timestamp (in seconds)*)
+    timezone by providing an implementation of the offset computation from both
+    a date and a timestamp (in seconds). *)
 val of_implementation :
   offset_calendar_time_s:(date:int * int * int -> time:int * int * int -> int) ->
   offset_timestamp_s:(unix_timestamp:int -> int) ->
@@ -27,24 +28,17 @@ val utc : t
 
 (** {1 Usage} *)
 
-(** When the timezone was created using [native] the offset depends on the given
-    time. It includes daylight savings where it applies. Static timezones yield
-    the same offset regardless of given time. *)
-
 (** [gmt_offset_seconds_with_datetime tz ~date ~time] is the number of seconds
-    that offset from UTC, for the given day (year, month, day) and time (hour,
+    that offset from UTC, for the given date (year, month, day) and time (hour,
     minute, second).
 
-    @raise Failure
-      if the native implementation finds that [date] and [time] do not exist in
-      [tz]. *)
+    Note: Implementations will struggle with consistency for ambiguous
+    dates. As an example: requesting the offset for an hour that is skipped
+    during a daylight saving transition will yeild an offset that may not agree
+    with {gmt_offset_seconds_with_ptime}.*)
 val gmt_offset_seconds_with_datetime :
   t -> date:int * int * int -> time:int * int * int -> int
 
 (** [gmt_offset_seconds_with_ptime tz timestamp] is the number of seconds that
-    offset from UTC at the time given by a unix timestamp (Ptime.t).
-
-    @raise Failure
-      if the native implementation finds that the [timestamp] do not exist in
-      [tz]. *)
+    offset from UTC, at the time given by a unix timestamp (Ptime.t). *)
 val gmt_offset_seconds_with_ptime : t -> Ptime.t -> int
