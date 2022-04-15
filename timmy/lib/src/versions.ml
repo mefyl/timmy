ea9de3b242e641613e8efa80126c04d40072a536
mefyl <mefyl@gruntech.org>
2022-04-15 13:54:48 +0200

    Prerelease 0.12.1.

--- timmy/lib/src/versions.ml
+++ timmy/lib/src/versions.ml
@@ -1,6 +1,6 @@
 (** Backward compatibility versions *)
 
-module V0_13_0 = struct
+module V0_12_1 = struct
   (** {1 Data types} *)
 
   (** A day of a month of a year. *)
@@ -26,4 +26,21 @@ module V0_13_0 = struct
 
   (** A day of the week. *)
   module Weekday = Weekday
+end
+
+module V0_12_0 = struct
+  include V0_12_1
+
+  module Timezone : sig
+    type t = Timezone.t
+
+    val of_gmt_offset_seconds : int -> t
+    val to_gmt_offset_seconds : t -> int
+    val utc : t
+  end = struct
+    include Timezone
+
+    let to_gmt_offset_seconds t =
+      gmt_offset_seconds_at_datetime ~date:(1970, 1, 1) ~time:(0, 0, 0) t
+  end
 end
