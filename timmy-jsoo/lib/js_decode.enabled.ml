c8c1de269aab3fe18ff78162bc3d1c0101394dff
mefyl <mefyl@gruntech.org>
2022-08-01 14:14:40 +0200

    Daytime: Export javascript interface.

--- timmy-jsoo/lib/js_decode.enabled.ml
+++ timmy-jsoo/lib/js_decode.enabled.ml
@@ -1,3 +1,10 @@
+module Daytime = struct
+  let to_js, of_js, of_js_exn =
+    Schematic_jsoo.Jsoo.helpers_typed
+      (module Timmy.Daytime)
+      "Timmy.Daytime.t" Timmy.Daytime.schema
+end
+
 module Week = struct
   let to_js, of_js, of_js_exn =
     Schematic_jsoo.Jsoo.helpers_typed
