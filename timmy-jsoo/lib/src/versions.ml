e689a8b24e559a04acb0b7342e3de3d54e847c60
mefyl <mefyl@gruntech.org>
2022-08-02 15:54:14 +0200

    Daytime: Actually export js interface.

--- timmy-jsoo/lib/src/versions.ml
+++ timmy-jsoo/lib/src/versions.ml
@@ -2,7 +2,7 @@
 
 module V0_13_1 = struct
   module Date = Date
-  module Daytime = Timmy.Daytime
+  module Daytime = Daytime
   module Month = Timmy.Month
   module Span = Timmy.Span
   module Time = Time
