076961cfadd33b88297646cf13a72a6d73f7b005
mefyl <mefyl@gruntech.org>
2022-08-19 12:12:41 +0200

    Export basic javascript library.

--- timmy-jsoo/tests/test.mjs
+++ timmy-jsoo/tests/test.mjs
@@ -1,8 +1,10 @@
-import caml from "./test.bc.js"
 import assert from "assert"
 
-const midnight = caml.make(0, 0, 0)
-const noon = caml.make(12, 0, 0)
+import timmy from "../bin/timmy.bc.js"
+const { Daytime } = timmy
+
+const midnight = new Daytime()
+const noon = new Daytime (12, 0, 0)
 assert(midnight.hours === 0)
 assert(midnight.minutes === undefined)
 assert(midnight.seconds === undefined)
@@ -10,10 +12,7 @@ assert(midnight.seconds === undefined)
 assert(midnight < noon, "daytime comparison failed")
 
 try {
-  const noon = caml.make(24, 0, 0)
+  new Daytime(24, 0, 0)
   assert(false, "invalid daytime not rejected")
 }
 catch (e) {}
-
-
-caml.testSuite()
