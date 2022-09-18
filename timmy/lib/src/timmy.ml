bacda0b58b6abc7408f50161be95f3c40fca72b8
mefyl <mefyl@gruntech.org>
2022-09-18 20:39:55 +0200

    Timmy: Remove deprecated rationale documentation.

--- timmy/lib/src/timmy.ml
+++ timmy/lib/src/timmy.ml
@@ -13,15 +13,3 @@ include Versions.V0_13_3
 (** {1 Backward compatibility} *)
 
 module Versions = Versions
-
-(** {1 Relation with Ptime and rationale}
-
-    {!Timmy} is entirely based on {!Ptime} and thus supports POSIX timestamps
-    with picosecond precision in a platform independent manner. It intends to
-    provide a slightly higher level interface, more type safety and convenience
-    functions.
-
-    - It is impossible to map dates to and from time points without specifying a
-      timezone. Many such Ptime functions have an optional timezone argument or
-      omit it entirely.
-    - Types are private records instead of tuples. This *)
