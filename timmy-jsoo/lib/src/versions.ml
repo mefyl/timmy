3c566239702530becdaf7e15ae94e4f1f1fd08ec
mefyl <mefyl@gruntech.org>
2022-10-11 18:19:03 +0200

    Prerelease 0.14.0.

--- timmy-jsoo/lib/src/versions.ml
+++ timmy-jsoo/lib/src/versions.ml
@@ -1,6 +1,6 @@
 (** Backward compatibility versions *)
 
-module V0_13_3 = struct
+module V0_14_0 = struct
   module Date = Date
   module Daytime = Daytime
   module Month = Timmy.Month
@@ -10,7 +10,3 @@ module V0_13_3 = struct
   module Week = Week
   module Weekday = Timmy.Weekday
 end
-
-module V0_13_2 = V0_13_3
-module V0_13_1 = V0_13_2
-module V0_13_0 = V0_13_1
