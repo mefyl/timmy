7a5fa072db5953a3bd708ea4ec78d9206dc20205
mefyl <mefyl@gruntech.org>
2022-10-17 10:26:10 +0200

    CI: Fix schematic-less tests.

--- timmy/tests/testable.ml
+++ timmy/tests/testable.ml
@@ -1,7 +1,3 @@
 let date = Alcotest.testable Timmy.Date.pp Timmy.Date.equal
 let daytime = Alcotest.testable Timmy.Daytime.pp Timmy.Daytime.equal
-
-let decoding_error =
-  Alcotest.testable Schematic.Error.pp_decoding Schematic.Error.equal_decoding
-
 let time = Alcotest.testable Timmy.Time.pp Timmy.Time.equal
