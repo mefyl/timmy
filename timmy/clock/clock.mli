abe8738d0efc94bf68d65f3a4b4a37247ddeb688
mefyl <mefyl@gruntech.org>
2022-10-23 18:19:46 +0200

    Document individual packages as well as the `Clock` library.

--- timmy/clock/clock.mli
+++ timmy/clock/clock.mli
@@ -1,2 +1,5 @@
+(** [now ()] is the current POSIX time, as returned by [Ptime_clock.now ()]. *)
 val now : unit -> Timmy.Time.t
+
+(** [timezone_local] is the local timezone. *)
 val timezone_local : Timmy.Timezone.t
