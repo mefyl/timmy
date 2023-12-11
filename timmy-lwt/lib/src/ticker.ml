8cfd9390f46377d4bb16b460c41b4c0ec82e7592
mefyl <mefyl@gruntech.org>
2023-12-11 13:47:21 +0100

    Timmy_lwt.Ticker: Add fallback finalisation on garbage collecting.

--- timmy-lwt/lib/src/ticker.ml
+++ timmy-lwt/lib/src/ticker.ml
@@ -19,12 +19,47 @@ type t = {
   skip : bool;
 }
 
-let _run t =
+module Weakref : sig
+  type 'a t
+
+  val make : 'a -> 'a t
+  val map : 'a t -> f:('a -> 'b) -> 'b option
+end = struct
+  type 'a t = 'a Stdlib.Weak.t
+
+  let make v =
+    let t = Stdlib.Weak.create 1 in
+    let () = Stdlib.Weak.set t 0 (Some v) in
+    t
+
+  let map t ~f =
+    match Stdlib.Weak.get t 0 with None -> None | Some v -> Some (f v)
+end
+
+let _run ticker =
+  let finalized, finalize = Lwt.wait () in
+  let () =
+    Stdlib.Gc.finalise_last (fun () -> Lwt.wakeup finalize Stop) ticker
+  in
+  let ticker = Weakref.make ticker in
   let ( >>= ) = Lwt.bind
   and ( >>| ) v f = Lwt.map f v in
+  let warn_not_finalised () =
+    let () =
+      Logs.err (fun m ->
+          m "@[<2>%a@]" Fmt.words
+            "Timmy_lwt.Ticker.t was not finalised. While resources are freed \
+             by this fallback upon garbage collection, it is not advised to \
+             rely upon it.")
+    in
+    Lwt.return ()
+  in
   let rec tick state =
-    let command = Lwt_mvar.take t.command in
-    tick_with_command command state
+    match Weakref.map ticker ~f:(fun { command; _ } -> command) with
+    | None -> warn_not_finalised ()
+    | Some command ->
+      let command = Lwt.choose [ Lwt_mvar.take command; finalized ] in
+      tick_with_command command state
   and tick_with_command command state =
     match state with
     | Paused { next_tick_at_pause } -> (
@@ -61,20 +96,23 @@ let _run t =
       | Some (Start (Some next_tick)) -> tick (Running { next_tick })
       | Some Pause -> tick (Paused { next_tick_at_pause = next_tick })
       | Some Stop -> tick Stopped
-      | None ->
-        let tick_time =
-          let skipped =
-            Timmy.Span.(Timmy.Time.(Clock.now () - next_tick) / t.period)
+      | None -> (
+        let tick ticker =
+          let tick_time =
+            let skipped =
+              Timmy.Span.(Timmy.Time.(Clock.now () - next_tick) / ticker.period)
+            in
+            if ticker.skip && Base.Int.(skipped > 0) then
+              Timmy.Time.(next_tick + Timmy.Span.(ticker.period * skipped))
+            else next_tick
           in
-          if t.skip && Base.Int.(skipped > 0) then
-            Timmy.Time.(next_tick + Timmy.Span.(t.period * skipped))
-          else next_tick
+          (ticker.f tick_time, Timmy.Time.(tick_time + ticker.period))
         in
-        let keep_going = t.f tick_time in
-        if keep_going then
-          tick_with_command command
-            (Running { next_tick = Timmy.Time.(tick_time + t.period) })
-        else tick_with_command command Stopped)
+        match Weakref.map ticker ~f:tick with
+        | None -> warn_not_finalised ()
+        | Some (true, next_tick) ->
+          tick_with_command command (Running { next_tick })
+        | Some (false, _) -> tick_with_command command Stopped))
   in
   Lwt.async (fun () -> tick Stopped)
 
@@ -98,16 +136,17 @@ let make ?(immediate = true) ?(skip = true) ?start:start_time ~period f =
   let () = _run ticker in
   ticker
 
-let finalise t = Lwt.async @@ fun () -> Lwt_mvar.put t.command Finalise
+let finalise ticker =
+  Lwt.async @@ fun () -> Lwt_mvar.put ticker.command Finalise
 
-let start ?start t =
+let start ?start ticker =
   Lwt.async @@ fun () ->
   match start with
-  | None -> Lwt_mvar.put t.command (Start None)
+  | None -> Lwt_mvar.put ticker.command (Start None)
   | Some start_time ->
-    Lwt_mvar.put t.command
-      (start_state ~immediate:t.immediate ~period:t.period start_time)
+    Lwt_mvar.put ticker.command
+      (start_state ~immediate:ticker.immediate ~period:ticker.period start_time)
 
-let stop t = Lwt.async @@ fun () -> Lwt_mvar.put t.command Stop
-let pause t = Lwt.async @@ fun () -> Lwt_mvar.put t.command Pause
+let stop ticker = Lwt.async @@ fun () -> Lwt_mvar.put ticker.command Stop
+let pause ticker = Lwt.async @@ fun () -> Lwt_mvar.put ticker.command Pause
 let skip { skip; _ } = skip
--- timmy-lwt/tests/ticker.ml
+++ timmy-lwt/tests/ticker.ml
@@ -1,6 +1,7 @@
 open Base
 
 let () = Timmy_lwt_virtual.install ()
+let () = Logs.set_reporter @@ Logs_fmt.reporter ()
 
 let immediate_on_start _ () =
   let count = ref 0 in
@@ -40,14 +41,16 @@ let skip _ () =
     Alcotest.(check ~here (list @@ testable Timmy.Time.pp Timmy.Time.equal))
       "skipped ticks" (exp exp_ticks_skipped) !ticks_skipped
   in
-  let _ticker = ticker ticks false
-  and _skip_ticker = ticker ticks_skipped true in
+  let ticker = ticker ticks false
+  and skip_ticker = ticker ticks_skipped true in
   let () = Clock_virtual.forward @@ Timmy.Span.seconds 30 in
   let () = check ~here:[%here] [ 0 ] [ 0 ] in
   let () = Clock_virtual.forward @@ Timmy.Span.minutes 1 in
   let () = check ~here:[%here] [ 1; 0 ] [ 1; 0 ] in
   let () = Clock_virtual.forward @@ Timmy.Span.minutes 5 in
   let () = check ~here:[%here] [ 6; 5; 4; 3; 2; 1; 0 ] [ 6; 1; 0 ] in
+  let () = Timmy_lwt.Ticker.finalise ticker
+  and () = Timmy_lwt.Ticker.finalise skip_ticker in
   Lwt.return ()
 
 let stop_resume _ () =
@@ -68,7 +71,21 @@ let stop_resume _ () =
   let () = Alcotest.(check ~here:[%here] int) "count" 4 !count in
   let () = Clock_virtual.forward @@ Timmy.Span.seconds 60 in
   let () = Alcotest.(check ~here:[%here] int) "count" 5 !count in
+  let () = Timmy_lwt.Ticker.finalise ticker in
   Lwt.return ()
+
+let forgotten_finalise _ () =
+  let errors = Logs.err_count () in
+  let () =
+    let _ticker =
+      Timmy_lwt.Ticker.make ~period:(Timmy.Span.minutes 1) (fun _ -> true)
+    in
+    ()
+  in
+  let () = Stdlib.Gc.full_major () in
+  Lwt.return
+  @@ Alcotest.(check ~here:[%here] int) "an error log was emitted" (errors + 1)
+  @@ Logs.err_count ()
 
 let () =
   Lwt_main.run
@@ -80,5 +97,6 @@ let () =
                test_case "not immediate on start" `Quick immediate_on_start;
                test_case "skip" `Quick skip;
                test_case "stop / resume" `Quick stop_resume;
+               test_case "forgotten finalise" `Quick forgotten_finalise;
              ] );
          ])
