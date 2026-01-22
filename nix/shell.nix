ae3bce511d68e6f18bc6e87b617186c467fc6205
mefyl <mefyl@gruntech.org>
2026-01-22 15:51:04 +0100

    Squashed '.logistic/' changes from 836b421..ee2bae9
    ee2bae9 CI: Switch to dune 3.21.0.
    3c36f80 CI: More ensuring that git describe is 7 characters long.
    e545569 Install dune stable version, through opam.
    b2ce828 Merge feature/nix-dune-pkg into master.
    46654da Simplify
    65a0180 Update dune, fix `dune pkg` flow for Nix
    f2cc9cd Upgrade dune builder image.
    129dfad Merge feature/nixpkgs-2025-12-29 into master.
    2d0e76c Update nixpkgs
    22ddec5 Define dune.lock lock dir manually.
    
    git-subtree-dir: .logistic
    git-subtree-split: ee2bae94b9c2df5002d23b893b699890356197e3

--- nix/shell.nix
+++ nix/shell.nix
@@ -8,46 +8,24 @@
   */
 , extraPackages ? (pkgs: [ ])
   /**
-    The path to a `.ocamlformat` file from which to extract the version.
-  */
-, dotOcamlformatPath ? null
-  /**
-    The ocamlformat version (formatted as Nix wants it).
-
-    Takes precedence over the one inferred from `dotOcamlformatPath`.
-  */
-, ocamlformatVersion ? null
-  /**
     Extra arguments passed directly to `mkShell`
   */
-, ocamlVersion
 , extraArgs ? { }
-} @ args:
+}:
 let
   ci-packages = import ./ci-packages.nix { inherit pkgs; };
 
-  dune-preview = (import srcs.flake-compat { src = srcs.dune; }).outputs.packages.${pkgs.system}.dune-experimental;
+  dune-preview = (import srcs.flake-compat { src = srcs.dune; }).outputs.packages.${pkgs.system}.dune;
   shared =
     import ./shared.nix { inherit pkgs; };
-  ocamlformatVersion_ =
-    if ocamlformatVersion != null then args.ocamlformatVersion else
-    if dotOcamlformatPath != null then
-      builtins.replaceStrings [ "." ] [ "_" ]
-        (shared.ocamlformatVersionFromDotOcamlformat dotOcamlformatPath)
-    else throw "One of 'ocamlformatVersion' or 'dotOcamlformatPath' must be supplied";
 in
 pkgs.mkShell
   ({
     packages = [
       # Base OCaml tooling
-      pkgs.ocaml-ng."ocamlPackages_${ocamlVersion}".ocaml
       dune-preview
-      pkgs.opam
 
       # Dev tooling
-      pkgs.ocaml-ng."ocamlPackages_${ocamlVersion}"."ocamlformat_${ocamlformatVersion_}"
-      pkgs.ocaml-ng."ocamlPackages_${ocamlVersion}".ocaml-lsp
-      pkgs.ocaml-ng."ocamlPackages_${ocamlVersion}".merlin
       shared.routine_run
 
       # System libraries
